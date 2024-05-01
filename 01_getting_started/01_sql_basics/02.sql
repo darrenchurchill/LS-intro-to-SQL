748a0bc06e80ef8db18ce0e752fb170a3307c08e
Darren Churchill <darrenmchurchill@gmail.com>
2024-04-30 17:44:16 -0700

    Add Ex 1.1.2

---
+++ 01_getting_started/01_sql_basics/02.sql
@@ -0,0 +1,15 @@
+/**
+ * Launch School Book Exercises
+ * Introduction to SQL
+ *
+ * Getting Started
+ * Chapter 1: SQL Basics Tutorial
+ * Exercise 2
+ *
+ * https://launchschool.com/books/sql/read/basics_tutorial#exercises
+ *
+ * Write a query that returns all of the orders that include a chocolate shake.
+ */
+
+-- for ls_burger db
+SELECT * FROM orders WHERE drink = 'Chocolate Shake';
