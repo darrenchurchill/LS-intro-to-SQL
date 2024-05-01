d2e33b4e698b9c667556f750c9440cc155a9b118
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-01 10:47:24 -0700

    Add Ex 2.2.1

---
+++ 02_schema/02_create_and_view_tables/01.bash
@@ -0,0 +1,14 @@
+# Launch School Book Exercises
+# Introduction to SQL
+#
+# Your First Database: Schema
+# Chapter 2: Create and View Tables
+# Exercise 2
+#
+# https://launchschool.com/books/sql/read/create_table#exercises
+#
+# From the terminal, create a database called `encyclopedia` and connect to it
+# via the psql console.
+
+createdb encyclopedia
+psql encyclopedia
