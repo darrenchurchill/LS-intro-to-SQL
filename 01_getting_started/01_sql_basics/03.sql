cffaa8d2d205b4fbd974327a63b5f2eb8d89333d
Darren Churchill <darrenmchurchill@gmail.com>
2024-04-30 17:52:19 -0700

    Add Ex 1.1.3

---
+++ 01_getting_started/01_sql_basics/03.sql
@@ -0,0 +1,16 @@
+/**
+ * Launch School Book Exercises
+ * Introduction to SQL
+ *
+ * Getting Started
+ * Chapter 1: SQL Basics Tutorial
+ * Exercise 3
+ *
+ * https://launchschool.com/books/sql/read/basics_tutorial#exercises
+ *
+ * Write a query that returns the burger, side, and drink for the order with an
+ * `id` of `2`.
+ */
+
+-- for ls_burger db
+SELECT burger, side, drink FROM orders WHERE id = 2;
