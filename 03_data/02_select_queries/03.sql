b40c1ce06711d27aa1ba92813d3754d492812a11
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-02 14:57:58 -0700

    Add Ex 3.2.3

---
+++ 03_data/02_select_queries/03.sql
@@ -0,0 +1,16 @@
+/**
+ * Launch School Book Exercises
+ * Introduction to SQL
+ *
+ * Your First Database: Data
+ * Chapter 2: Select Queries
+ * Exercise 3
+ *
+ * https://launchschool.com/books/sql/read/select_queries#exercises
+ *
+ * Write a query to return the names of all the countries ordered
+ * alphabetically.
+ */
+
+-- for encyclopedia db
+SELECT name FROM countries ORDER BY name;
