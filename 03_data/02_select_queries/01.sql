444b152b4c65284dbf99c46b9a0e6c88c419b9bb
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-02 14:54:03 -0700

    Add Ex 3.2.1

---
+++ 03_data/02_select_queries/01.sql
@@ -0,0 +1,16 @@
+/**
+ * Launch School Book Exercises
+ * Introduction to SQL
+ *
+ * Your First Database: Data
+ * Chapter 2: Select Queries
+ * Exercise 1
+ *
+ * https://launchschool.com/books/sql/read/select_queries#exercises
+ *
+ * Make sure you are connected to the `encyclopedia` database. Write a query to
+ * retrieve the population of the USA.
+ */
+
+-- for encyclopedia db
+SELECT population FROM countries WHERE name = 'USA';
