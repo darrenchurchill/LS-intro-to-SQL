0615aecc19a277424862a069ba98343207c5bdb7
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-02 15:08:46 -0700

    Add Ex 3.2.7

---
+++ 03_data/02_select_queries/07.sql
@@ -0,0 +1,16 @@
+/**
+ * Launch School Book Exercises
+ * Introduction to SQL
+ *
+ * Your First Database: Data
+ * Chapter 2: Select Queries
+ * Exercise 7
+ *
+ * https://launchschool.com/books/sql/read/select_queries#exercises
+ *
+ * Write a query that returns the names of all the countries with a population
+ * greater than 70 million.
+ */
+
+-- for encyclopedia db
+SELECT name FROM countries WHERE population > 70000000;
