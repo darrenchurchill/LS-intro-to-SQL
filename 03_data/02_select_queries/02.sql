2371c80974dcc7406dee01852276ff052b257df3
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-02 14:55:54 -0700

    Add Ex 3.2.2

---
+++ 03_data/02_select_queries/02.sql
@@ -0,0 +1,16 @@
+/**
+ * Launch School Book Exercises
+ * Introduction to SQL
+ *
+ * Your First Database: Data
+ * Chapter 2: Select Queries
+ * Exercise 2
+ *
+ * https://launchschool.com/books/sql/read/select_queries#exercises
+ *
+ * Write a query to return the population and the capital (with the columns in
+ * that order) of all the countries in the table.
+ */
+
+-- for encyclopedia db
+SELECT population, capital FROM countries;
