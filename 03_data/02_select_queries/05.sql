2fb1a9f2c3d7728e55e1732124b47d2ca711a9a9
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-02 15:01:10 -0700

    Add Ex 3.2.5

---
+++ 03_data/02_select_queries/05.sql
@@ -0,0 +1,16 @@
+/**
+ * Launch School Book Exercises
+ * Introduction to SQL
+ *
+ * Your First Database: Data
+ * Chapter 2: Select Queries
+ * Exercise 5
+ *
+ * https://launchschool.com/books/sql/read/select_queries#exercises
+ *
+ * Write a query to return the names and capitals of all the countries in order
+ * by population, from highest to lowest.
+ */
+
+-- for encyclopedia db
+SELECT name, capital FROM countries ORDER BY population DESC;
