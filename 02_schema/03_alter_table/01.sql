61b2fe592dde1981b9730549153711695efaabfb
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-01 17:27:44 -0700

    Add Ex 2.3.1

---
+++ 02_schema/03_alter_table/01.sql
@@ -0,0 +1,16 @@
+/**
+ * Launch School Book Exercises
+ * Introduction to SQL
+ *
+ * Your First Database: Schema
+ * Chapter 3: Alter a Table
+ * Exercise 1
+ *
+ * https://launchschool.com/books/sql/read/alter_table#exercises
+ *
+ * Make sure you are connected to the `encyclopedia` database. Rename the
+ * `famous_people` table to `celebrities`.
+ */
+
+-- for encyclopedia db
+ALTER TABLE famous_people RENAME TO celebrities;
