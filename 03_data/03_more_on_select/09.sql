6ce3373b7722be2cb9e9e540e5fac70581320790
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-04 16:31:27 -0700

    Add Ex 3.3.9

---
+++ 03_data/03_more_on_select/09.sql
@@ -0,0 +1,18 @@
+/**
+ * Launch School Book Exercises
+ * Introduction to SQL
+ *
+ * Your First Database: Data
+ * Chapter 3: More on Select
+ * Exercise 9
+ *
+ * https://launchschool.com/books/sql/read/more_on_select#exercises
+ *
+ * Write a query to return the number of rows in the `countries` table.
+ */
+-- for encyclopedia db
+SELECT
+  count(*)
+FROM
+  countries
+;
