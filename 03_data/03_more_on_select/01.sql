d8bdeb7a881ac4786d2d90b6949359a1f0caffb7
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-04 16:00:38 -0700

    Add Ex 3.3.1

---
+++ 03_data/03_more_on_select/01.sql
@@ -0,0 +1,21 @@
+/**
+ * Launch School Book Exercises
+ * Introduction to SQL
+ *
+ * Your First Database: Data
+ * Chapter 3: More on Select
+ * Exercise 1
+ *
+ * https://launchschool.com/books/sql/read/more_on_select#exercises
+ *
+ * Make sure you are connected to the `encyclopedia` database. Write a query to
+ * retrieve the fist row of data from the `countries` table.
+ */
+-- for encyclopedia db
+SELECT
+  *
+FROM
+  countries
+LIMIT
+  1
+;
