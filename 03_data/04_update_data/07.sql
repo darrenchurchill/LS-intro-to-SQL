0b8a035501f91a3c626326921c90ed1bef5af013
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-05 11:55:24 -0700

    Add Ex 3.4.7

---
+++ 03_data/04_update_data/07.sql
@@ -0,0 +1,17 @@
+/**
+ * Launch School Book Exercises
+ * Introduction to SQL
+ *
+ * Your First Database: Data
+ * Chapter 4: Update Data
+ * Exercise 7
+ *
+ * https://launchschool.com/books/sql/read/update_and_delete_data#exercises
+ *
+ * Remove all the rows from the `countries` table
+ */
+-- @block
+-- @conn encyclopedia
+-- @label Delete all rows from countries
+DELETE FROM countries
+;
